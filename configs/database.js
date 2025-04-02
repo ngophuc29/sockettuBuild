516c6f577b03fbd56eff3948b1868b97a77d72c0
ngophuc29 <ngophuc2911@gmail.com>
2025-04-02 21:41:36 +0700

    chinh config

--- configs/database.js
+++ configs/database.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/demoChatSocket")
+        await mongoose.connect("mongodb+srv://ngophuc2911:phuc29112003@cluster0.9wvzd5u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        // mongodb+srv://ngophuc2911:<db_password>@cluster0.9wvzd5u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+        // mongodb://localhost:27017/demoChatSocket
         console.log("Connect to database successfully")
         
     } catch (error) {
--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const { Server } = require('socket.io');
 
 const cors = require("cors");
 app.use(cors({
-    origin: "http://localhost:3000"
+    origin: "*"
 }));
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "*",
         methods: ["GET", "POST"]
     }
 });
